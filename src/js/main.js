01ab8d28d6d6e02ec9fab5247e4ca234b753f489
Mason Chinkin <mason.chinkin@gmail.com>
2019-03-26 13:28:15 -0700

    Line chart labels fixed

--- src/js/main.js
+++ src/js/main.js
@@ -14,18 +14,18 @@ let key = 0 //initialize for highlighting purposes; gets redefines as 'income', 
 const highlightTransition = 50
 const newYearTransition = 800
 
-let thisYear = 2017
+window.thisYear = 2017
 
 // load the data
 export const drawDasboard = () => {
-  // d3.csv('../../data/us-budget-sankey-main.csv').then(csv => { // main
-  //   d3.csv('../../data/us-budget-sankey-deficit.csv').then(deficit => { // deficit
-  //     d3.csv('../../data/us-budget-sankey-bars.csv').then(barData => { // bars
+  d3.csv('../../data/us-budget-sankey-main.csv').then(csv => { // main
+    d3.csv('../../data/us-budget-sankey-deficit.csv').then(deficit => { // deficit
+      d3.csv('../../data/us-budget-sankey-bars.csv').then(barData => { // bars
 
-  // absolute path for github pages
-  d3.csv('data/us-budget-sankey-main.csv').then(csv => { // main
-    d3.csv('data/us-budget-sankey-deficit.csv').then(deficit => { // deficit
-      d3.csv('data/us-budget-sankey-bars.csv').then(barData => { // bars
+        // absolute path for github pages
+        // d3.csv('data/us-budget-sankey-main.csv').then(csv => { // main
+        //   d3.csv('data/us-budget-sankey-deficit.csv').then(deficit => { // deficit
+        //     d3.csv('data/us-budget-sankey-bars.csv').then(barData => { // bars
 
         newData(csv, deficit, thisYear)
         drawBars(barData)
@@ -421,10 +421,11 @@ function drawSlider() {
     .tickFormat(d3.format('.4'))
     .default(2017)
     .on('end', thisYear => { //use end instead of onchange, is when user releases mouse
-      // d3.csv('../../data/us-budget-sankey-main.csv').then(csv => {
-      //   d3.csv('../../data/us-budget-sankey-deficit.csv').then(deficit => {
-      d3.csv('data/us-budget-sankey-main.csv').then(csv => {
-        d3.csv('data/us-budget-sankey-deficit.csv').then(deficit => {
+      window.thisYear = thisYear
+      d3.csv('../../data/us-budget-sankey-main.csv').then(csv => {
+        d3.csv('../../data/us-budget-sankey-deficit.csv').then(deficit => {
+          // d3.csv('data/us-budget-sankey-main.csv').then(csv => {
+          //   d3.csv('data/us-budget-sankey-deficit.csv').then(deficit => {
 
           //update
           d3.select('.deficit').remove()
@@ -436,6 +437,7 @@ function drawSlider() {
       })
     })
     .on('onchange', thisYear => { //use end instead of onchange, is when user releases mouse
+      window.thisYear = thisYear
       updateBars(thisYear)
       updateThisYearLine(thisYear)
     })
@@ -705,13 +707,14 @@ function highlight() {
     .selectAll('text')
     .data(lineLabelData)
     .enter()
-
     .append('text')
     .filter((d, i) => i === 0 || i === (lineLabelData.length - 1) || d.year === thisYear)
     .attr('x', (d, i) => (d.type == 'Revenue') ? revLineX(d.year) : spendLineX(d.year))
     .attr('y', d => lineY(d.value) - 14)
     .text((d, i) => formatNumber(d.value))
     .attr('class', 'lineLabel')
+
+  console.log(thisYear)
 }
 
 const onlyUnique = (value, index, self) => self.indexOf(value) === index
